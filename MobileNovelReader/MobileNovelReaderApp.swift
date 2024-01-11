a27f8af68cbf75edec0c4ff6e2834efc6a7da6f4
taku ikegami <takuron1996@gmail.com>
2024-01-12 07:43:27 +0900

    Initial Commit

---
+++ MobileNovelReader/MobileNovelReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileNovelReaderApp.swift
+//  MobileNovelReader
+//
+//  Created by 池上拓 on 2024/01/12.
+//
+
+import SwiftUI
+
+@main
+struct MobileNovelReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
