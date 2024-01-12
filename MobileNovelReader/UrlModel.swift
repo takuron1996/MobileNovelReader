f0221c00f935a4c2187fcded8496c83bc4235015
taku ikegami <takuron1996@gmail.com>
2024-01-12 16:37:56 +0900

    TODOと暗黙のオプショナル型を使用している理由を追加

--- MobileNovelReader/UrlModel.swift
+++ MobileNovelReader/UrlModel.swift
@@ -9,8 +9,9 @@ import Foundation
 
 struct UrlModel{
     var url: URL! {
-        // 後で外部APIからURLを取得
+        // TODO: 後で外部APIからURLを取得
         let url = "https://ncode.syosetu.com/n0091ip/1/"
+        // urlの設定箇所の都合でnilになることはないため、暗黙のオプショナル型で宣言
         return URL(string:url)!
     }
 }
