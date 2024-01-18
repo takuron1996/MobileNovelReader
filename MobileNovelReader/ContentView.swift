2644d7f3c77499770d30534cda02ecbf36a0b137
taku ikegami <takuron1996@gmail.com>
2024-01-19 07:46:33 +0900

    目次の代用としてContentViewを使用

--- MobileNovelReader/ContentView.swift
+++ MobileNovelReader/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var fetcher = MainTextFetcher()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        //TODO 一旦目次の代わりに使用
+        NavigationStack{
+            NavigationLink(destination: NovelDisplayView(ncode: "n0902ip", episode: 1).environmentObject(fetcher)) {
+                Text("第1話")
+            }
+            NavigationLink(destination: NovelDisplayView(ncode: "n0902ip", episode: 2).environmentObject(fetcher)) {
+                Text("第2話")
+            }
+            NavigationLink(destination: NovelDisplayView(ncode: "n0902ip", episode: 3).environmentObject(fetcher)) {
+                Text("第3話")
+            }
         }
-        .padding()
     }
 }
 
