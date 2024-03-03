bf34196503c10f77588ef75bf367ad648f6e39dc
taku ikegami <takuron1996@gmail.com>
2024-03-03 17:32:41 +0900

    エラー用のデータを作成

---
+++ MobileNovelReader/Common/ErrorData.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorData.swift
+//  MobileNovelReader
+//
+//  Created by 池上拓 on 2024/03/01.
+//
+
+import Foundation
+
+struct ErrorData: Codable{
+    var error: String
+    var errorDescription: String
+}
